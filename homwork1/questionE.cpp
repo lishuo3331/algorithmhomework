cc4f26f92561e7a6d49ff218d6c43b3c5d2599ff
lishuo3331 <2016201211@hrbeu.edu.cn>
2020-10-20 17:09:59 +0800

    2020年10月20日17点09分QE

--- homwork1/questionE.cpp
+++ homwork1/questionE.cpp
@@ -1,49 +1,73 @@
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
-
+#include <cstdio>
 using namespace std;
 
-int isPrime(int num)
+// int isPrime(int num)
+// {
+// 	if(num ==2 || num ==3)
+// 	{
+// 		return 1;
+// 	}
+// 	if (num%6 != 1 || num %6 !=5)
+// 	{
+// 		return 0;
+// 	}
+// 	int tmp = sqrt(num);
+// 	for (int i = 0; i < tmp; i+=6)
+// 	{
+// 		if(num%i == 0 || num%(i+2) == 0)
+// 		{
+// 			return 0;
+// 		}
+// 	}
+// 	return 1;
+// }
+bool isPrime(int x)
 {
-	if(num ==2 || num ==3)
+    if(x==0||x==1) 
+    	return 0;
+    for(int i=2;i<=sqrt(x);i++)
+    	if(x%i==0) 
+    		return 0;
+    return 1;
+}
+// void dfs(int k,int ans,int n)
+// {
+//     if(k==n)
+//     {
+//         if(pd(ans))
+//         {
+//             printf("%d",ans);
+//             puts("");
+//         }
+//     }
+//     for(int i=0;i<=9;i++)
+//     {
+//         if(pd(ans*10+i)) 
+//         dfs(k+1,ans*10+i,n);
+//     }
+//     return;
+// }
+
+void answerE(int N,int pass)
+{
+	if (N==0)
 	{
-		return 1;
+		cout << pass << endl;
 	}
-	if (num%6 != 1 || num %6 !=5)
-	{
-		return 0;
-	}
-	int tmp = sqrt(num) + 1;
-	for (int i = 0; i < tmp; i+=6)
-	{
-		if(num%i == 0 || num%(i+2) == 0)
+	for (int i = 0; i <= 9;i++)
+	{	
+		if (isPrime(pass*10 + i))
 		{
-			return 0;
+			answerE(N-1,pass*10 + i);
 		}
-	}
-	return 1;
+		}
+	return ;
 }
 
-int answerE(int N,int pass)
-{
-	// int number = 0;
-	if (N > 0)
-	{
-		for (int i = 2; i <=9; i++)
-		{
-			number = i;
-			if(isPrime(number))
-			{
-				// cout << number;
-				number = answerE(N,number) + pass * 10;
-			}
-		}
-	}
 	
-	N--;
-	return number;
-}
 int main(int argc, char const *argv[])
 {
 	int N;
