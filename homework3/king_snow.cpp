dbcce95ac5d8e9e1882e8b3fb729e268bfb8a3b3
huluchuan <36464694+lishuo3331@users.noreply.github.com>
2020-11-05 23:38:29 +0800

    2020年11月5日23点38分
    king_snow

--- homework3/king_snow.cpp
+++ homework3/king_snow.cpp
@@ -1,46 +1,59 @@
 #include <iostream>
 #include <algorithm>
-
+//https://www.cnblogs.com/yalphait/p/8535495.html
 using namespace std;
+struct node
+{
+	int x,y;
+	int height;
+};
 int h_set[505][505];
 int result[505][505];
-int max(int x,int y,int z,int m)
+node node_list[250005];
+
+int comp(const void *q,const void *w)
 {
-	return 0;
-}
-int next_step(int x,int y)
-{
-	return 0;
+	return ((struct node*)q)->height - ((struct node*)w)->height;
 }
 int main(int argc, char const *argv[])
 {
+	int direr[4] = {0,0,-1,1}, direl[4] = {-1,1,0,0};
 	int R,C;
+	cin >> R >> C;
+	int index = 0;
 	for(int i = 1;i <= R;++i)
 	{
 		for(int j = 1;j <=C;++j)
 		{
 			cin >> h_set[i][j];
+			node_list[index].x = i;
+			node_list[index].y = j;
+			node_list[index].height = h_set[i][j];
+			index++;
 		}
-		h_set[0][i] = 1000;
-		h_set[i][0] = 1000;
-		h_set[R+1][i] = 1000;
-		h_set[i][R+1] = 1000;
 	}
-	for(int i = 1;i <= R;++i)
+	qsort(node_list,index,sizeof(struct node),comp);
+	for(int i=0;i<index;i++)
 	{
-		h_set[1][i] = 1;
-		h_set[i][1] = 1;
+		for (int j = 0; j <= 3; ++j)
+		{
+			if (h_set[node_list[i].x + direr[j]][node_list[i].y + direl[j]] > h_set[node_list[i].x][node_list[i].y])
+			{
+				result[node_list[i].x + direr[j]][node_list[i].y + direl[j]] = max(result[node_list[i].x + direr[j]][node_list[i].y + direl[j]],result[node_list[i].x][node_list[i].y]+1);
+			}
+		}
 	}
+	int answer = 0;
 	for (int i = 1; i <= R; ++i)
 	{
 		for (int j = 1; j <= C; ++j)
 		{
-			if (result[i][j] < result[i][j-1])
+			if (answer <= result[i][j])
 			{
-				result[i][j] +=result[i][j-1];
+				answer = result[i][j];
 			}
 		}
-		for(int j <= C;)	
 	}
+	cout << answer+1 <<endl;
 	return 0;
 }
